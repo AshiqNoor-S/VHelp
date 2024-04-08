9f971b9b399011a28c92c956b1f87b78cccb2189
AshiqNoor-S <ashiqnoorsudheer@gmail.com>
2024-04-08 19:15:10 +0530

    first push

---
+++ vhelp/src/components/header/Nav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles/nav.css"
+
+const Nav = () => (
+    <header className='navbar'>
+        <div className='navbar__title navbar__item'>Cutco</div>
+        <div className='navbar__item'>About Us</div>
+        <div className='navbar__item'>Contact</div>
+        <div className='navbar__item'>Help</div>        
+    </header>
+);
+
+export  default Nav; 
